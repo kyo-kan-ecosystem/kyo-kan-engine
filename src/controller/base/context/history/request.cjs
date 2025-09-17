2a086db4d563085697c5cb08b4dbc610a391b2f7
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2025-09-18 03:17:13 +0900

    refacter
    apply update

--- src/controller/base/context/history/request.cjs
+++ src/controller/base/context/history/request.cjs
@@ -1,23 +1,5 @@
 const { MapedHistory } = require("./maped_history.cjs");
 
-class RequestHistory extends MapedHistory {
-    addNewLog(request, branchId) {
-
-        const branchHeader = this.getBranchHead(branchId, false);
-        if (branchHeader != request) {
-            const id = super.addNewLog(branchId, request)
-
-
-
-        }
-        else {
-            super.addNonUpdateLog(branchId)
-
-        }
-
-
-
-    }
-}
+class RequestHistory extends MapedHistory { }
 
 module.exports = { RequestHistory }    
