dfaa1ef4461f4c0b05e5095df5769a8284d852db
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2025-10-09 08:52:53 +0900

    update gosub logic

--- src/controller/base/context/index.cjs
+++ src/controller/base/context/index.cjs
@@ -143,7 +143,7 @@ class Context {
     goSub() {
 
 
-        this.states.push({})
+        this.states.push()
 
 
         this.bords.push()
--- src/controller/base/context/states/states.cjs
+++ src/controller/base/context/states/states.cjs
@@ -3,7 +3,21 @@ const { StackTree } = require('../../../../util/stack.cjs');
 
 
 
-class States extends StackTree { }
+class States extends StackTree {
+    /**
+     * @param {*} data
+     */
+    push(data) {
+        let _data
+        if (typeof data == 'undefined' || data === null) {
+            _data = {}
+        }
+        else {
+            _data = data
+        }
+        return super.push(_data)
+    }
+}
 
 
 module.exports = { States }
